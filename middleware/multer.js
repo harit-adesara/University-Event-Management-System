13a2e127405f03582024859a2b9f319ba43d6ef7
Harit-01 <harit13soni@gmail.com>
2026-03-23 21:08:00 +0530

    filter for image upload

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,3 +1,13 @@
 import multer from "multer";
 
-export const upload = multer({ storage: multer.memoryStorage() });
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
+    if (!allowedTypes.includes(file.mimetype)) {
+      return cb(new multer.MulterError("Only insert JPEG,PNG or WEBP"));
+    }
+    cb(null, true);
+  },
+});
